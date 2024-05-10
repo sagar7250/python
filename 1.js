0885a618069e70c447206c5ff649f9183e8887b9
sagar7250 <149947109+sagar7250@users.noreply.github.com>
2024-05-10 16:18:54 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,18 @@
+// function person (name,age){
+//     this.name=name;
+//     this.age=age;
+//     this.changename=cn;
+// }
+// function cn(name){
+//     this.name=name
+// }
+// var p1=new person("sagar",23);
+// console.log(p1.age)
+// console.log(p1.name)
+// p1.changename("reddu")
+// console.log(p1.name,p1.age)
+
+var n1=new Array(10,1,1,1,2,2,2,3,3,3,4,4,4,5,6,7)
+console.log(n1)
+n1.pop()
+console.log(n1)
